45c2c4e4c0e1adf5aa45ce8209db5bf3affb404e
JorisH <joris@tactics.be>
2023-06-14 14:13:31 +0200

    added samesite cookie support

--- lib/storage/sfSessionStorage.class.php
+++ lib/storage/sfSessionStorage.class.php
@@ -4,7 +4,7 @@
  * This file is part of the symfony package.
  * (c) 2004-2006 Fabien Potencier <fabien.potencier@symfony-project.com>
  * (c) 2004-2006 Sean Kerr <sean@code-box.org>
- * 
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
@@ -25,122 +25,124 @@
  */
 class sfSessionStorage extends sfStorage
 {
-  /**
-   * Initializes this Storage instance.
-   *
-   * @param sfContext A sfContext instance
-   * @param array   An associative array of initialization parameters
-   *
-   * @return boolean true, if initialization completes successfully, otherwise false
-   *
-   * @throws <b>sfInitializationException</b> If an error occurs while initializing this Storage
-   */
-  public function initialize($context, $parameters = null)
-  {
-    // initialize parent
-    parent::initialize($context, $parameters);
-
-    // set session name
-    $sessionName = $this->getParameterHolder()->get('session_name', 'symfony');
-
-    session_name($sessionName);
-
-    $use_cookies = (boolean) ini_get('session.use_cookies');
-    if (!$use_cookies)
+    /**
+     * Initializes this Storage instance.
+     *
+     * @param sfContext A sfContext instance
+     * @param array   An associative array of initialization parameters
+     *
+     * @return boolean true, if initialization completes successfully, otherwise false
+     *
+     * @throws <b>sfInitializationException</b> If an error occurs while initializing this Storage
+     */
+    public function initialize($context, $parameters = null)
     {
-      $sessionId = $context->getRequest()->getParameter($sessionName, '');
+        // initialize parent
+        parent::initialize($context, $parameters);
 
-      if ($sessionId != '')
-      {
-        session_id($sessionId);
-      }
+        // set session name
+        $sessionName = $this->getParameterHolder()->get('session_name', 'symfony');
+
+        session_name($sessionName);
+
+        $use_cookies = (boolean)ini_get('session.use_cookies');
+        if (!$use_cookies) {
+            $sessionId = $context->getRequest()->getParameter($sessionName, '');
+
+            if ($sessionId != '') {
+                session_id($sessionId);
+            }
+        }
+
+        $cookieDefaults = session_get_cookie_params();
+        $lifetime = $this->getParameter('session_cookie_lifetime', $cookieDefaults['lifetime']);
+        $path = $this->getParameter('session_cookie_path', $cookieDefaults['path']);
+        $domain = $this->getParameter('session_cookie_domain', $cookieDefaults['domain']);
+        $secure = $this->getParameter('session_cookie_secure', $cookieDefaults['secure']);
+        $httpOnly = $this->getParameter('session_cookie_httponly', isset($cookieDefaults['httponly']) ? $cookieDefaults['httponly'] : false);
+        $sameSite = $this->getParameter('session_cookie_samesite', isset($cookieDefaults['samesite']) ? $cookieDefaults['samesite'] : 'lax');
+        if (version_compare(phpversion(), '7.3', '>=')) {
+            session_set_cookie_params([
+                'lifetime' => $lifetime,
+                'path' => $path,
+                'domain' => $domain,
+                'secure' => $secure,
+                'httponly' => $httpOnly,
+                'samesite' => $sameSite
+            ]);
+        } elseif (version_compare(phpversion(), '5.2', '>=')) {
+            session_set_cookie_params($lifetime, $path, $domain, $secure, $httpOnly);
+        } else {
+            session_set_cookie_params($lifetime, $path, $domain, $secure);
+        }
+
+        if ($this->getParameter('auto_start', true)) {
+            // start our session
+            session_start();
+        }
     }
 
-    $cookieDefaults = session_get_cookie_params();
-    $lifetime = $this->getParameter('session_cookie_lifetime', $cookieDefaults['lifetime']);
-    $path     = $this->getParameter('session_cookie_path',     $cookieDefaults['path']);
-    $domain   = $this->getParameter('session_cookie_domain',   $cookieDefaults['domain']);
-    $secure   = $this->getParameter('session_cookie_secure',   $cookieDefaults['secure']);
-    $httpOnly = $this->getParameter('session_cookie_httponly', isset($cookieDefaults['httponly']) ? $cookieDefaults['httponly'] : false);
-    if (version_compare(phpversion(), '5.2', '>='))
+    /**
+     * Reads data from this storage.
+     *
+     * The preferred format for a key is directory style so naming conflicts can be avoided.
+     *
+     * @param string A unique key identifying your data
+     *
+     * @return mixed Data associated with the key
+     */
+    public function & read($key)
     {
-      session_set_cookie_params($lifetime, $path, $domain, $secure, $httpOnly);
-    }
-    else
-    {
-      session_set_cookie_params($lifetime, $path, $domain, $secure);
-    }
+        $retval = null;
 
-    if ($this->getParameter('auto_start', true))
-    {
-      // start our session
-      session_start();
-    }
-  }
+        if (isset($_SESSION[$key])) {
+            $retval =& $_SESSION[$key];
+        }
 
-  /**
-   * Reads data from this storage.
-   *
-   * The preferred format for a key is directory style so naming conflicts can be avoided.
-   *
-   * @param string A unique key identifying your data
-   *
-   * @return mixed Data associated with the key
-   */
-  public function & read($key)
-  {
-    $retval = null;
-
-    if (isset($_SESSION[$key]))
-    {
-      $retval =& $_SESSION[$key];
+        return $retval;
     }
 
-    return $retval;
-  }
-
-  /**
-   * Removes data from this storage.
-   *
-   * The preferred format for a key is directory style so naming conflicts can be avoided.
-   *
-   * @param string A unique key identifying your data
-   *
-   * @return mixed Data associated with the key
-   */
-  public function & remove($key)
-  {
-    $retval = null;
-
-    if (isset($_SESSION[$key]))
+    /**
+     * Removes data from this storage.
+     *
+     * The preferred format for a key is directory style so naming conflicts can be avoided.
+     *
+     * @param string A unique key identifying your data
+     *
+     * @return mixed Data associated with the key
+     */
+    public function & remove($key)
     {
-      $retval =& $_SESSION[$key];
-      unset($_SESSION[$key]);
+        $retval = null;
+
+        if (isset($_SESSION[$key])) {
+            $retval =& $_SESSION[$key];
+            unset($_SESSION[$key]);
+        }
+
+        return $retval;
     }
 
-    return $retval;
-  }
+    /**
+     * Writes data to this storage.
+     *
+     * The preferred format for a key is directory style so naming conflicts can be avoided.
+     *
+     * @param string A unique key identifying your data
+     * @param mixed  Data associated with your key
+     *
+     */
+    public function write($key, &$data)
+    {
+        $_SESSION[$key] =& $data;
+    }
 
-  /**
-   * Writes data to this storage.
-   *
-   * The preferred format for a key is directory style so naming conflicts can be avoided.
-   *
-   * @param string A unique key identifying your data
-   * @param mixed  Data associated with your key
-   *
-   */
-  public function write($key, &$data)
-  {
-    $_SESSION[$key] =& $data;
-  }
-
-  /**
-   * Executes the shutdown procedure.
-   *
-   */
-  public function shutdown()
-  {
-    // don't need a shutdown procedure because read/write do it in real-time
-  }
+    /**
+     * Executes the shutdown procedure.
+     *
+     */
+    public function shutdown()
+    {
+        // don't need a shutdown procedure because read/write do it in real-time
+    }
 }
