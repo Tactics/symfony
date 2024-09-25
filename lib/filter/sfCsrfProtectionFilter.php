de72738f5a0f1c0a9f8d6a3828b04cf6abf512a7
JorisH <joris@tactics.be>
2024-09-25 15:04:10 +0200

    added CsrfProtected attribute

--- lib/filter/sfCsrfProtectionFilter.php
+++ lib/filter/sfCsrfProtectionFilter.php
@@ -8,24 +8,84 @@ class sfCsrfProtectionFilter extends sfFilter
         $request = $context->getRequest();
         $controller = $context->getController();
         $actionEntry = $controller->getActionStack()->getLastEntry();
+        /** @var sfAction $actionInstance */
         $actionInstance = $actionEntry->getActionInstance();
 
-        if ($request->hasParameter(sfCsrfTokenManager::SESSION_KEY_FIELD_NAME)) {
-            $formName = $request->getParameter(sfCsrfTokenManager::SESSION_KEY_FIELD_NAME);
-            $csrfManager = new sfCsrfTokenManager($formName);
-            $token = $request->getParameter(sfCsrfTokenManager::TOKEN_FIELD_NAME);
-            if (!$csrfManager->validateToken(sfCsrfTokenManager::TOKEN_FIELD_NAME, $token)) {
-                $request->setError('csrf', 'Ongeldige CSRF token. Probeer het opnieuw.');
-
-                $actionName = $context->getActionName();
-                $handleErrorToRun = 'handleError'.ucfirst($actionName);
-                $viewName = method_exists($actionInstance, $handleErrorToRun) ? $actionInstance->$handleErrorToRun() : $actionInstance->handleError();
-                if ($viewName === '')
-                {
-                    $viewName = sfView::ERROR;
+        $actionIsCsrfProtected = $this->actionIsCsrfProtected($actionInstance);
+        if (!$actionIsCsrfProtected) {
+            // no CsrfProtected attribute but _name is in payload
+            if ($request->hasParameter(sfCsrfTokenManager::SESSION_KEY_FIELD_NAME)) {
+                if (!$this->hasValidToken($request)) {
+                    // prevent forwarding loop if only _name is present or _name with an invalid token
+                    $request->getParameterHolder()->remove(sfCsrfTokenManager::SESSION_KEY_FIELD_NAME);
+                    $this->forward404MissingOrInvalidCsrfToken($request, $controller);
                 }
             }
+            // we end this filter here
+            $filterchain->execute();
+            // we just return from the filter on the way back
+            return;
         }
+
+        // Action is CSRF protected so there MUST be a valid token
+        if (!$request->hasParameter(sfCsrfTokenManager::SESSION_KEY_FIELD_NAME)
+            || !$request->hasParameter(sfCsrfTokenManager::TOKEN_FIELD_NAME)
+            || !$this->hasValidToken($request)) {
+            $this->forward404MissingOrInvalidCsrfToken($request, $controller);
+        }
+
         $filterchain->execute();
+    }
+
+    /**
+     * @param sfAction $actionInstance
+     * @return bool
+     * @throws ReflectionException
+     */
+    private function actionIsCsrfProtected(sfAction $actionInstance): bool
+    {
+        $attributes = [];
+        $validateAction = 'validate' . ucfirst($actionInstance->getActionName());
+        if (method_exists($actionInstance, $validateAction)) {
+            $refValidateMethod = new ReflectionMethod($actionInstance::class, $validateAction);
+            $attributes = array_merge($attributes, $refValidateMethod->getAttributes(CsrfProtected::class));
+        }
+
+        $executeAction = 'execute' . ucfirst($actionInstance->getActionName());
+        if (method_exists($actionInstance, $executeAction)) {
+            $refExecuteMethod = new ReflectionMethod($actionInstance::class, $executeAction);
+            $attributes = array_merge($attributes, $refExecuteMethod->getAttributes(CsrfProtected::class));
+        }
+
+        return !empty($attributes);
+    }
+
+    /**
+     * @param sfRequest $request
+     * @param sfController $controller
+     * @return mixed
+     * @throws sfConfigurationException
+     * @throws sfForwardException
+     * @throws sfInitializationException
+     * @throws sfStopException
+     */
+    private function forward404MissingOrInvalidCsrfToken(sfRequest $request, sfController $controller)
+    {
+        $request->setParameter('message', 'Invalid or missing CSRF token!');
+        $controller->forward(sfConfig::get('sf_error_404_module'), sfConfig::get('sf_error_404_action'));
+        throw new sfStopException();
+    }
+
+    /**
+     * @param sfRequest $request
+     * @return bool
+     */
+    private function hasValidToken(sfRequest $request): bool
+    {
+        $formName = $request->getParameter(sfCsrfTokenManager::SESSION_KEY_FIELD_NAME);
+        $csrfManager = new sfCsrfTokenManager($formName);
+        $token = $request->getParameter(sfCsrfTokenManager::TOKEN_FIELD_NAME);
+
+        return $csrfManager->validateToken(sfCsrfTokenManager::TOKEN_FIELD_NAME, $token);
     }
 }
